8d9e646ca7ac8f6a250ed968aa57033088f7ad81
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-01-02 10:20:43 -0300

    Projeto criado

---
+++ src/main/java/com/lucasmonteiro/cursomc/ImplementacaoMcComspringBootEJpaApplication.java
@@ -0,0 +1,13 @@
+package com.lucasmonteiro.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImplementacaoMcComspringBootEJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImplementacaoMcComspringBootEJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucasmonteiro/cursomc/ImplementacaoMcComspringBootEJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasmonteiro.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImplementacaoMcComspringBootEJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
