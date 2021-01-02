10b855dd7ee98ff5dd2117bb07d0ad4c8589dbff
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-01-02 11:40:23 -0300

    Testando a primeira classe de domínio: Categoria

---
+++ src/main/java/com/lucasmonteiro/cursomc/domain/Categoria.java
@@ -0,0 +1,62 @@
+package com.lucasmonteiro.cursomc.domain;
+
+import java.io.Serializable;
+
+public class Categoria implements Serializable { // Serializable é uma interface que diz que os objetos da classe poderão ser convertidos para uma sequência de bits. Isso serve para que os onjetos possam ser gravados em arquivos, trafegar em redes, etc
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String nome;
+	
+	public Categoria() {} // Instancia um objeto sem jogar nada para os atributos
+	
+	public Categoria(Integer id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	// hashCode e equals são operações para comparar os objetos por valor
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+}
--- src/main/java/com/lucasmonteiro/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/lucasmonteiro/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,29 @@
 package com.lucasmonteiro.cursomc.resources; // resources --> É onde são gravadas as classes que são: controladores REST
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.lucasmonteiro.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias") // No value é colocado o nome do endpoint REST
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando!";
+	public List<Categoria> listar() {
+		
+		Categoria categoria1 = new Categoria(1, "Informática");
+		Categoria categoria2 = new Categoria(2, "Games");
+		
+		List<Categoria> categorias = new ArrayList<>();
+		categorias.add(categoria1);
+		categorias.add(categoria2);
+		
+		return categorias;
 	}
 
 }
