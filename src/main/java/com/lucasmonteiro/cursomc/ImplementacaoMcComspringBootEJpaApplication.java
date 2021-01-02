c2e69d9263edab26fc43cedeccb6d7186fd2dca3
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-01-02 17:54:55 -0300

    Criando operacao de instanciacao

--- src/main/java/com/lucasmonteiro/cursomc/ImplementacaoMcComspringBootEJpaApplication.java
+++ src/main/java/com/lucasmonteiro/cursomc/ImplementacaoMcComspringBootEJpaApplication.java
@@ -1,13 +1,33 @@
 package com.lucasmonteiro.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ImplementacaoMcComspringBootEJpaApplication {
+import com.lucasmonteiro.cursomc.domain.Categoria;
+import com.lucasmonteiro.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class ImplementacaoMcComspringBootEJpaApplication implements CommandLineRunner { // CommandLineRunner permite implementar um método auxiliar para executar alguma ação quando a aplicação iniciar
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ImplementacaoMcComspringBootEJpaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria categoria1 = new Categoria(null, "Informática"); // O id é null pois o banco de dados vai informar o id automaticamente
+		Categoria categoria2 = new Categoria(null, "Games");
+		
+		categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2));
+		
 	}
 
 }
